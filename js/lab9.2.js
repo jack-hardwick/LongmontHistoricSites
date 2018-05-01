34bc9ba44ab6dc3014f7b4618a68040d4690b780
jack-hardwick <35667938+jack-hardwick@users.noreply.github.com>
2018-05-01 14:13:59 -0600

    Making it Pretty
    Updating CSS

--- js/lab9.2.js
+++ js/lab9.2.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var map = L.map('map').setView([40.1634608, -105.100107], 14);
-
+    map.zoomControl.setPosition('topright');
+    
     L.tileLayer('https://api.mapbox.com/styles/v1/jack-hardwick/cjef3ijq04jh82snpvezi7ydo/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFjay1oYXJkd2ljayIsImEiOiJjamVmMmFhc2IxemQxMnBsbmI1dW5qbjg5In0.LA81ed1j-W5il_-0zUm1zA', 
                 {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -34,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function myOnEachFeature(feature, layer) {
     var li = document.createElement("li"),
         a = document.createElement("Button"),
-        content = feature.properties.No + ". " + feature.properties.Name;
+        content = feature.properties.Name;
         siteList.push({No: feature.properties.No, Name:feature.properties.Name, Location: feature.properties.Location, Date: feature.properties.Date, Year_Desig: feature.properties.Year_Desig, webpage: feature.properties.WEB, Lat: feature.properties.Lat, Long: feature.properties.Long, imgSrc: feature.properties.img_Src})
         
     // Create the "button".
@@ -44,10 +45,22 @@ document.addEventListener('DOMContentLoaded', function() {
     a.onclick = function myFunction()
         {
             var siteNum = a.layer.feature.properties.No;
-            document.getElementById("cardText").innerHTML = siteList[siteNum-1].webpage;
-            document.getElementById("sitePic").src = siteList[siteNum-1].imgSrc;
+            document.getElementById("cardText").href = a.layer.feature.properties.WEB;
+            document.getElementById("cardText").innerHTML = a.layer.feature.properties.WEB;
+            if(a.layer.feature.properties.img_Src == "")
+                {
+                   document.getElementById("sitePic").alt = "No Picture Available!"; 
+                }
+            else{
+                document.getElementById("sitePic").src =a.layer.feature.properties.img_Src;
+                document.getElementById("sitePic").alt = a.layer.feature.properties.Name;
+            } 
             var siteLoc = L.latLng(a.layer.feature.properties.Lat, a.layer.feature.properties.Long);
             map.flyTo(siteLoc, 18);
+            var popup = L.popup()
+            .setLatLng(siteLoc)
+            .setContent(a.layer.feature.properties.Name)
+            .openOn(map);
         }
    
         /*
@@ -68,6 +81,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     console.log(siteList);
     layer.bindPopup("Name: " + content + "Lat:" + feature.properties.Lat + " Long: " + feature.properties.Long + " Image Source: " + feature.properties.img_Src);
+    
 }
 
 /*    
