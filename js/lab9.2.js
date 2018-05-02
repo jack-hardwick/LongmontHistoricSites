14fb674768297442c9ede073bc7e01a36b2a453e
jack-hardwick <35667938+jack-hardwick@users.noreply.github.com>
2018-05-02 10:02:27 -0600

    Added Comments
    Unfortunately, I think this is the best I can do with the time I had to put into this. I am happy that most of the functionality is available. I have my gripes concerning CSS not wanting to update or work on a regular basis though.

--- js/lab9.2.js
+++ js/lab9.2.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //create the map and set zoom controls to the upper right portion of the screen.
     var map = L.map('map').setView([40.1634608, -105.100107], 14);
     map.zoomControl.setPosition('topright');
 
+    //import custom mapbox basemap
     L.tileLayer('https://api.mapbox.com/styles/v1/jack-hardwick/cjef3ijq04jh82snpvezi7ydo/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamFjay1oYXJkd2ljayIsImEiOiJjamVmMmFhc2IxemQxMnBsbmI1dW5qbjg5In0.LA81ed1j-W5il_-0zUm1zA', 
                 {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18
     }).addTo(map);  
-
+    
+    //unsure if this list element is still needed, but I am hesitant to remove anything at this point in fear of breaking it.
     var list = document.getElementById("list");
     var siteList = [];
-
+    
+    //call feature layer data from AGO
     var sites = L.esri.featureLayer({
         url: 'https://services.arcgis.com/YseQBnl2jq0lrUV5/arcgis/rest/services/latlongsites/FeatureServer/0',
 
@@ -21,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }).addTo(map);
 
-
+    //for each feature, create a button populated with the name, that onclick() flys the camera to the location of that point.
     function myOnEachFeature(feature, layer) {
         // Create the "button" and List.
         var li = document.createElement("li"),
@@ -32,13 +36,15 @@ document.addEventListener('DOMContentLoaded', function() {
         
 
         a.innerHTML = content;
-        a.layer = layer; // Store a reference to the actual layer.
+        a.layer = layer; // Store a reference to the actual layer. This was what I should have been paying attention to. Though I swore I tried referencing this earlier with the buttons to no avail. 
         a.onclick = function myFunction()
         {
+            //update the card info 
             var siteNum = a.layer.feature.properties.No;
             document.getElementById("cardText").href = a.layer.feature.properties.WEB;
             document.getElementById("cardText").innerHTML = a.layer.feature.properties.WEB;
-            if(a.layer.feature.properties.img_Src == "")
+            //A number of the sites do not have images currently available. This just makes the alt text that shows up in the empty box a littl more relavent. 
+            if(a.layer.feature.properties.img_Src == "No Picture Available!")
             {
                 document.getElementById("sitePic").alt = "No Picture Available!"; 
             }
@@ -48,35 +54,28 @@ document.addEventListener('DOMContentLoaded', function() {
             } 
             var siteLoc = L.latLng(a.layer.feature.properties.Lat, a.layer.feature.properties.Long);
             map.flyTo(siteLoc, 18);
-            //This doesn't want to work for whatever reason... Should open a popup on button click. Tried multiple permutations. Probably a dumb mistake somewhere.
-            var popup = L.popup();
-            popup.setLatLng(siteLoc)
-            .setcontent("Site: " + a.layer.feature.properties.Name)
-            .openPopup(popup)
-
+            
+            //The popup doesn't want to work for whatever reason... Should open a popup on button click. Tried multiple permutations. Probably a dumb mistake somewhere. Now even having this code uncommented
+            //will lead to a critical error resulting in nothing displaying on the map at all. Apparently there is an unexpected token??
+            
+            //var popup = L.popup();
+            //.setLatLng(siteLoc)
+            //.setcontent("Site: " + a.layer.feature.properties.Name)
+            //.openPopup(popup)
+            //According to the Leaflet documentation, the four lines above should be all I need. 
         }
 
-        /*
-        a.onclick = function (event) {
-        event.preventDefault(); // Prevent the link from scrolling the page.
-        //map.flyToBounds(feature.geometry.coordinates);
-
-        var siteLoc = L.latLng(a.layer.feature.properties.Lat, a.layer.feature.properties.Long);
-        //map.setVeiw(siteLoc, 16);
-        //console.log(feature.properties);
-        document.getElementByClassName("container").innerHTML = feature.properties.webpage;
-        layer.openPopup();
-    };
-    */
-
+        //add the newly created button to the list
         li.appendChild(a);
         list.appendChild(li);
 
         console.log(siteList);
-        layer.bindPopup("Name: " + content + "Lat:" + feature.properties.Lat + " Long: " + feature.properties.Long + " Image Source: " + feature.properties.img_Src);
+        layer.bindPopup("<h3>Name: " + content + "</h3><h3>Lat:" + feature.properties.Lat + "\n Long: " + feature.properties.Long + "</h3><img style=width:100px src='" + feature.properties.img_Src + "'>");
 
     }
-
+    
+    //extra function that I like, Maybe not entirely necessary however..
+    //though it does function as a closePopup() feature. This means any click off screen will close any other opened popups. Unitentional but I like it.
     function onMapClick(e) 
     {
         var popup = L.popup()
